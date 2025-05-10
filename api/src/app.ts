8061ec13eb219e958d635e436064a636a5477743
Érico Ribeiro <oliv.ericorib@gmail.com>
2025-05-10 10:34:26 -0300

    feat: setup express.js

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,1 +1,8 @@
-console.log('Olá, Mundo!');
+import express from 'express';
+
+const app = express();
+
+const host: string = String(process.env.SERVER_HOST);
+const port: number = Number(process.env.SERVER_PORT);
+
+app.listen(port, host, () => console.info(`Servidor rodando em ${host}:${port}`));
