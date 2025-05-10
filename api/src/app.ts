bd7d145ea74967fe11d8591a863d3ef5ac15aaa6
Érico Ribeiro <oliv.ericorib@gmail.com>
2025-05-10 10:48:08 -0300

    feat: add index router

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 import { serverConfig } from './config/server.config';
+import { router } from './router';
 
 const app = express();
+app.use(express.json());
+app.use(router);
 
 const { port, host, url } = serverConfig;
 
