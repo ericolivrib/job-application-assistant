d8c0d103d6001b1d21c920da178005ebf75b84b8
Érico Ribeiro <oliv.ericorib@gmail.com>
2025-05-11 12:58:39 -0300

    feat: encrypt user password when created

--- api/src/model/user.model.ts
+++ api/src/model/user.model.ts
@@ -1,10 +1,30 @@
 import mongoose from "mongoose";
+import bcrypt from 'bcrypt';
 
 const UserSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    password:  String,
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password:  {
+        type: String,
+        required: true
+    },
     skills: [String],
+});
+
+UserSchema.pre('save', async function (next) {
+    if (this.isModified('password')) {
+        return next();
+    }
+
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
 });
 
 const User = mongoose.model('User', UserSchema);
